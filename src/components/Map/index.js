478fc7a5f16b718f967b7edf39e0ac975ddec3d5
Oksana Shukh <oksana@zephyros.design>
2024-01-08 16:41:30 +0200

    update marker

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, Icon } from "react-leaflet";
 import React from "react";
 import "leaflet/dist/leaflet.css";
+import { icon } from "leaflet";
+
+// import icon from "../../assets/images/marker-shadow.png";
 
 const Map = ({ data }) => {
   const [coordinates, setCoordinates] = useState([6, 34]);
@@ -13,8 +16,14 @@ const Map = ({ data }) => {
       setCoordinates([lat, lng]);
     }
   }, [data]);
+  const customIcon = icon({
+    iconUrl: "../../assets/images/marker-shadow.png",
+    iconSize: [50, 50],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -30],
+  });
   return (
-    <div className="mt-3">
+    <div className="mt-3 map-container">
       <MapContainer
         center={coordinates}
         zoom={13}
@@ -24,7 +33,7 @@ const Map = ({ data }) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={coordinates}></Marker>
+        <Marker position={coordinates} icon={customIcon}></Marker>
       </MapContainer>
     </div>
   );
